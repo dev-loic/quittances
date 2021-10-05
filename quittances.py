5c425cc0b4bdf13bd639c9aa8fb8053da12dd752
loic.saillant <loic.saillant.dev@gmail.com>
2021-10-05 17:55:55 +0200

    Send empty email for now with the right config address

--- quittances.py
+++ quittances.py
@@ -1,6 +1,10 @@
 from __future__ import print_function
 import os.path
 import sys
+import json
+import base64
+from requests import HTTPError
+from email.mime.text import MIMEText
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -11,7 +15,7 @@ import locale
 locale.setlocale(locale.LC_TIME,'fr_FR')
 
 # LOCAL
-from errors import ArgumentsError
+from errors import ArgumentsError, Error
 
 ## GLOBAL VARIABLES
 TODAY = date.today()
@@ -19,31 +23,37 @@ TODAY = date.today()
 # !!! If modifying these scopes, delete the file token.json.
 SCOPES = [
     'https://www.googleapis.com/auth/documents',
-    'https://www.googleapis.com/auth/drive'
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/gmail.send'
 ]
-
-TEMPLATE_ID = '1B2Fw3U51_L7GxDG9DQwKbwTmyPAk8P39bezC52oQEeE'
 
 def main():
     try:
         if len(sys.argv) != 2:
             raise ArgumentsError
 
-        folder_name = sys.argv[1]
+        (template_id, tenant_email) = retrieve_infos(sys.argv[1])
+
         creds = retrieve_and_persist_credentials()
         docs_service = build('docs', 'v1', credentials=creds)
         drive_service = build('drive', 'v3', credentials=creds)
+        gmail_service = build('gmail', 'v1', credentials=creds)
 
-        copy_id = copy_template(drive_service)
+        copy_id = copy_template(drive_service, template_id)
         if copy_id is not None:
             edit_document(docs_service, copy_id)
             copy_document = retrieve_document(docs_service, copy_id)
             print('La quittance {} a été correctement créée 🎉🎉🎉'.format(copy_document.get('title')))
 
+        send_email(gmail_service, tenant_email)
+
     except ArgumentsError:
         print('❌ Nom du dossier contenant les informations absent')
-    except:
+    except HTTPError as error:
+        print(error)
+    except Error as error:
         print('❌ Error durant la création du document')
+        print(error)
 
 
 ## CREDENTIALS
@@ -70,16 +80,23 @@ def retrieve_and_persist_credentials():
 
     return creds
 
+## INFOS
+def retrieve_infos(folder_name):
+    file_name = "input_infos/{}/infos.json".format(folder_name)
+    with open(file_name) as json_file:
+        data = json.load(json_file)
+        return (data['template_id'], data['tenant_email'])
+
 ## DOCS
 def retrieve_document(docs_service, id):
     return docs_service.documents().get(documentId=id).execute()
 
-def copy_template(drive_service):
+def copy_template(drive_service, template_id):
     copy_title = get_title()
     body = {
         'name': copy_title
     }
-    drive_response = drive_service.files().copy(fileId=TEMPLATE_ID, body=body).execute()
+    drive_response = drive_service.files().copy(fileId=template_id, body=body).execute()
     return drive_response.get('id', None)
 
 ## EDIT / CUSTOMIZING
@@ -147,6 +164,17 @@ def get_period():
 def get_fees(monthly_total=110.00, rental_charge=7.12):
     monthly_fee = monthly_total - rental_charge
     return (str(monthly_fee), str(rental_charge), str(monthly_total))
+
+
+## SEND EMAIL
+def send_email(gmail_service, tenant_email):
+    message = MIMEText('Ce message est vide 🥸')
+    message['to'] = tenant_email
+    # Loic Saillant: get_title() should be replace with the real title of the file
+    message['subject'] = "Quittance {}".format(get_title())
+    final = {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+
+    message = gmail_service.users().messages().send(userId='me', body=final).execute()
 
 if __name__ == '__main__':
     main()
