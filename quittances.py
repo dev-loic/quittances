efc936fb05023a137e5c83277002adfc918ffe6d
loic.saillant <loic.saillant.dev@gmail.com>
2021-10-05 16:46:17 +0200

    Create all the modifying methods to generate a new completed doc

--- quittances.py
+++ quittances.py
@@ -5,9 +5,14 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from datetime import date
+from calendar import monthrange
 import locale
+locale.setlocale(locale.LC_TIME,'fr_FR')
 
-# If modifying these scopes, delete the file token.json.
+## GLOBAL VARIABLES
+TODAY = date.today()
+
+# !!! If modifying these scopes, delete the file token.json.
 SCOPES = [
     'https://www.googleapis.com/auth/documents',
     'https://www.googleapis.com/auth/drive'
@@ -16,14 +21,20 @@ SCOPES = [
 TEMPLATE_ID = '1B2Fw3U51_L7GxDG9DQwKbwTmyPAk8P39bezC52oQEeE'
 
 def main():
-    creds = retrieve_and_persist_credentials()
-    docs_service = build('docs', 'v1', credentials=creds)
-    drive_service = service = build('drive', 'v3', credentials=creds)
+    try:
+        creds = retrieve_and_persist_credentials()
+        docs_service = build('docs', 'v1', credentials=creds)
+        drive_service = build('drive', 'v3', credentials=creds)
 
-    copy_id = copy_template(drive_service)
-    if copy_id is not None:
-        copy_document = retrieve_document(docs_service, copy_id)
-        print('The title of the document is: {}'.format(copy_document.get('title')))
+        copy_id = copy_template(drive_service)
+        if copy_id is not None:
+            edit_document(docs_service, copy_id)
+            copy_document = retrieve_document(docs_service, copy_id)
+            print('La quittance {} a été correctement créée 🎉🎉🎉'.format(copy_document.get('title')))
+
+    except:
+        print('❌ Error durant la création du document')
+
 
 ## CREDENTIALS
 def retrieve_and_persist_credentials():
@@ -62,10 +73,70 @@ def copy_template(drive_service):
     return drive_response.get('id', None)
 
 ## EDIT / CUSTOMIZING
+def edit_document(docs_service, id):
+    (monthly_fee, rental_charge, monthly_total) = get_fees()
+    requests = [
+        {
+            'replaceAllText': {
+                'containsText': {
+                    'text': '{{period}}',
+                    'matchCase':  'true'
+                },
+                'replaceText': get_period()
+            }
+        },
+        {
+            'replaceAllText': {
+                'containsText': {
+                    'text': '{{monthly-fee}}',
+                    'matchCase':  'true'
+                },
+                'replaceText': monthly_fee
+            }
+        },
+        {
+            'replaceAllText': {
+                'containsText': {
+                    'text': '{{rental-charge}}',
+                    'matchCase':  'true'
+                },
+                'replaceText': rental_charge
+            }
+        },
+        {
+            'replaceAllText': {
+                'containsText': {
+                    'text': '{{monthly-total}}',
+                    'matchCase':  'true'
+                },
+                'replaceText': monthly_total
+            }
+        },
+        {
+            'replaceAllText': {
+                'containsText': {
+                    'text': '{{today-date}}',
+                    'matchCase':  'true'
+                },
+                'replaceText': TODAY.strftime('%d/%m/%y')
+            }
+        }
+    ]
+    result = docs_service.documents().batchUpdate(
+        documentId=id, body={'requests': requests}).execute()
+
 def get_title():
-    locale.setlocale(locale.LC_TIME,'fr_FR')
-    today = date.today()
-    return today.strftime("%B_%Y")
+    return TODAY.strftime("%B_%Y")
+
+def get_period():
+    current_month_nb_of_days = monthrange(TODAY.year, TODAY.month)
+    first_day = "1er {}".format(TODAY.strftime("%B %Y"))
+    last_day = "{} {}".format(current_month_nb_of_days[1], TODAY.strftime("%B %Y"))
+    return "{} au {}".format(first_day, last_day)
+
+def get_fees(monthly_total=110.00, rental_charge=7.12):
+    monthly_fee = monthly_total - rental_charge
+    return (str(monthly_fee), str(rental_charge), str(monthly_total))
 
 if __name__ == '__main__':
     main()
